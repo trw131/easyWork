30608d598ebd67b1a29b89d4c22d6b70f77431c4
RyanTian <tianrenwu43@gmail.com>
2026-02-05 14:19:41 +0800

    remove build artifacts

--- easyWork/MainView.Designer.cs
+++ easyWork/MainView.Designer.cs
@@ -87,69 +87,69 @@
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.Root = this.layoutControlGroup1;
-            this.layoutControl1.Size = new System.Drawing.Size(900, 600);
+            this.layoutControl1.Size = new System.Drawing.Size(700, 500);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
             // lblCurrentDate
             // 
-            this.lblCurrentDate.Appearance.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Bold);
+            this.lblCurrentDate.Appearance.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
             this.lblCurrentDate.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
             this.lblCurrentDate.Appearance.Options.UseFont = true;
             this.lblCurrentDate.Appearance.Options.UseForeColor = true;
-            this.lblCurrentDate.Location = new System.Drawing.Point(15, 15);
+            this.lblCurrentDate.Location = new System.Drawing.Point(12, 12);
             this.lblCurrentDate.Name = "lblCurrentDate";
-            this.lblCurrentDate.Size = new System.Drawing.Size(252, 26);
+            this.lblCurrentDate.Size = new System.Drawing.Size(216, 22);
             this.lblCurrentDate.StyleController = this.layoutControl1;
             this.lblCurrentDate.TabIndex = 4;
             this.lblCurrentDate.Text = "2026年02月05日 星期四";
             // 
             // memoEditLog
             // 
-            this.memoEditLog.Location = new System.Drawing.Point(15, 50);
+            this.memoEditLog.Location = new System.Drawing.Point(12, 42);
             this.memoEditLog.Name = "memoEditLog";
             this.memoEditLog.Properties.Appearance.BackColor = System.Drawing.Color.White;
-            this.memoEditLog.Properties.Appearance.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.memoEditLog.Properties.Appearance.Font = new System.Drawing.Font("微软雅黑", 9.5F);
             this.memoEditLog.Properties.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
             this.memoEditLog.Properties.Appearance.Options.UseBackColor = true;
             this.memoEditLog.Properties.Appearance.Options.UseFont = true;
             this.memoEditLog.Properties.Appearance.Options.UseForeColor = true;
             this.memoEditLog.Properties.AcceptsReturn = true;
             this.memoEditLog.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.memoEditLog.Properties.Padding = new System.Windows.Forms.Padding(8);
+            this.memoEditLog.Properties.Padding = new System.Windows.Forms.Padding(6);
             this.memoEditLog.Properties.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.memoEditLog.Properties.WordWrap = true;
             this.memoEditLog.Properties.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
-            this.memoEditLog.Size = new System.Drawing.Size(870, 475);
+            this.memoEditLog.Size = new System.Drawing.Size(676, 398);
             this.memoEditLog.StyleController = this.layoutControl1;
             this.memoEditLog.TabIndex = 5;
             // 
             // btnExport
             // 
             this.btnExport.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
-            this.btnExport.Appearance.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.btnExport.Appearance.Font = new System.Drawing.Font("微软雅黑", 8.5F);
             this.btnExport.Appearance.ForeColor = System.Drawing.Color.White;
             this.btnExport.Appearance.Options.UseBackColor = true;
             this.btnExport.Appearance.Options.UseFont = true;
             this.btnExport.Appearance.Options.UseForeColor = true;
-            this.btnExport.Location = new System.Drawing.Point(15, 538);
+            this.btnExport.Location = new System.Drawing.Point(12, 450);
             this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(110, 40);
+            this.btnExport.Size = new System.Drawing.Size(95, 36);
             this.btnExport.StyleController = this.layoutControl1;
             this.btnExport.TabIndex = 6;
             this.btnExport.Text = "导出月度报表";
             // 
-            // btnHistory   
+            // btnHistory
             // 
             this.btnHistory.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
-            this.btnHistory.Appearance.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.btnHistory.Appearance.Font = new System.Drawing.Font("微软雅黑", 8.5F);
             this.btnHistory.Appearance.ForeColor = System.Drawing.Color.White;
             this.btnHistory.Appearance.Options.UseBackColor = true;
             this.btnHistory.Appearance.Options.UseFont = true;
             this.btnHistory.Appearance.Options.UseForeColor = true;
-            this.btnHistory.Location = new System.Drawing.Point(135, 538);
+            this.btnHistory.Location = new System.Drawing.Point(115, 450);
             this.btnHistory.Name = "btnHistory";
-            this.btnHistory.Size = new System.Drawing.Size(110, 40);
+            this.btnHistory.Size = new System.Drawing.Size(95, 36);
             this.btnHistory.StyleController = this.layoutControl1;
             this.btnHistory.TabIndex = 7;
             this.btnHistory.Text = "查看历史";
@@ -157,30 +157,30 @@
             // btnCopy
             // 
             this.btnCopy.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
-            this.btnCopy.Appearance.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.btnCopy.Appearance.Font = new System.Drawing.Font("微软雅黑", 8.5F);
             this.btnCopy.Appearance.ForeColor = System.Drawing.Color.White;
             this.btnCopy.Appearance.Options.UseBackColor = true;
             this.btnCopy.Appearance.Options.UseFont = true;
             this.btnCopy.Appearance.Options.UseForeColor = true;
-            this.btnCopy.Location = new System.Drawing.Point(255, 538);
+            this.btnCopy.Location = new System.Drawing.Point(218, 450);
             this.btnCopy.Name = "btnCopy";
-            this.btnCopy.Size = new System.Drawing.Size(110, 40);
+            this.btnCopy.Size = new System.Drawing.Size(95, 36);
             this.btnCopy.StyleController = this.layoutControl1;
             this.btnCopy.TabIndex = 8;
             this.btnCopy.Text = "复制今日日志";
             // 
             // lblLastSaved
             // 
-            this.lblLastSaved.Appearance.Font = new System.Drawing.Font("微软雅黑", 8.5F);
+            this.lblLastSaved.Appearance.Font = new System.Drawing.Font("微软雅黑", 8F);
             this.lblLastSaved.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
             this.lblLastSaved.Appearance.Options.UseFont = true;
             this.lblLastSaved.Appearance.Options.UseForeColor = true;
             this.lblLastSaved.Appearance.Options.UseTextOptions = true;
             this.lblLastSaved.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.lblLastSaved.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
-            this.lblLastSaved.Location = new System.Drawing.Point(735, 538);
+            this.lblLastSaved.Location = new System.Drawing.Point(558, 450);
             this.lblLastSaved.Name = "lblLastSaved";
-            this.lblLastSaved.Size = new System.Drawing.Size(150, 40);
+            this.lblLastSaved.Size = new System.Drawing.Size(130, 36);
             this.lblLastSaved.StyleController = this.layoutControl1;
             this.lblLastSaved.TabIndex = 9;
             this.lblLastSaved.Text = "最后保存: 14:30:25";
@@ -198,8 +198,8 @@
             this.layoutControlItem6,
             this.emptySpaceItem1});
             this.layoutControlGroup1.Name = "layoutControlGroup1";
-            this.layoutControlGroup1.Padding = new DevExpress.XtraLayout.Utils.Padding(13, 13, 13, 13);
-            this.layoutControlGroup1.Size = new System.Drawing.Size(900, 600);
+            this.layoutControlGroup1.Padding = new DevExpress.XtraLayout.Utils.Padding(10, 10, 10, 10);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(700, 500);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // layoutControlItem1
@@ -207,76 +207,76 @@
             this.layoutControlItem1.Control = this.lblCurrentDate;
             this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(874, 33);
+            this.layoutControlItem1.Size = new System.Drawing.Size(680, 28);
             this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem1.TextVisible = false;
-            this.layoutControlItem1.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 2, 2, 5);
+            this.layoutControlItem1.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 2, 2, 4);
             // 
             // layoutControlItem2
             // 
             this.layoutControlItem2.Control = this.memoEditLog;
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 33);
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 28);
             this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(874, 489);
+            this.layoutControlItem2.Size = new System.Drawing.Size(680, 408);
             this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem2.TextVisible = false;
-            this.layoutControlItem2.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 2, 2, 10);
+            this.layoutControlItem2.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 2, 2, 8);
             // 
             // layoutControlItem3
             // 
             this.layoutControlItem3.Control = this.btnExport;
-            this.layoutControlItem3.Location = new System.Drawing.Point(0, 522);
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 436);
             this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(120, 52);
+            this.layoutControlItem3.Size = new System.Drawing.Size(103, 44);
             this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem3.TextVisible = false;
-            this.layoutControlItem3.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 8, 2, 2);
+            this.layoutControlItem3.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 6, 2, 2);
             // 
             // layoutControlItem4
             // 
             this.layoutControlItem4.Control = this.btnHistory;
-            this.layoutControlItem4.Location = new System.Drawing.Point(120, 522);
+            this.layoutControlItem4.Location = new System.Drawing.Point(103, 436);
             this.layoutControlItem4.Name = "layoutControlItem4";
-            this.layoutControlItem4.Size = new System.Drawing.Size(120, 52);
+            this.layoutControlItem4.Size = new System.Drawing.Size(103, 44);
             this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem4.TextVisible = false;
-            this.layoutControlItem4.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 8, 2, 2);
+            this.layoutControlItem4.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 6, 2, 2);
             // 
             // layoutControlItem6
             // 
             this.layoutControlItem6.Control = this.btnCopy;
-            this.layoutControlItem6.Location = new System.Drawing.Point(240, 522);
+            this.layoutControlItem6.Location = new System.Drawing.Point(206, 436);
             this.layoutControlItem6.Name = "layoutControlItem6";
-            this.layoutControlItem6.Size = new System.Drawing.Size(120, 52);
+            this.layoutControlItem6.Size = new System.Drawing.Size(103, 44);
             this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem6.TextVisible = false;
-            this.layoutControlItem6.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 8, 2, 2);
+            this.layoutControlItem6.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 6, 2, 2);
             // 
             // layoutControlItem5
             // 
             this.layoutControlItem5.Control = this.lblLastSaved;
-            this.layoutControlItem5.Location = new System.Drawing.Point(720, 522);
+            this.layoutControlItem5.Location = new System.Drawing.Point(546, 436);
             this.layoutControlItem5.Name = "layoutControlItem5";
-            this.layoutControlItem5.Size = new System.Drawing.Size(154, 52);
+            this.layoutControlItem5.Size = new System.Drawing.Size(134, 44);
             this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem5.TextVisible = false;
             // 
             // emptySpaceItem1
             // 
             this.emptySpaceItem1.AllowHotTrack = false;
-            this.emptySpaceItem1.Location = new System.Drawing.Point(360, 522);
+            this.emptySpaceItem1.Location = new System.Drawing.Point(309, 436);
             this.emptySpaceItem1.Name = "emptySpaceItem1";
-            this.emptySpaceItem1.Size = new System.Drawing.Size(360, 52);
+            this.emptySpaceItem1.Size = new System.Drawing.Size(237, 44);
             this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
             // 
             // MainView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(700, 500);
             this.Controls.Add(this.layoutControl1);
             this.IconOptions.ShowIcon = false;
-            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "MainView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EasyWork - 工作日志记录";
